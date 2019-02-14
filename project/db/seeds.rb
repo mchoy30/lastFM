aea6b2f99765ab8a170c7b106059502fd4def0c2
Mackenzie <mchoy30@academic.rrc.ca>
2019-02-14 15:05:00 -0600

    Added faker data to seed script

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+Song.destroy_all
+Album.destroy_all
+
 new_album = Album.create(artist: 'Say anything', album_title: 'Is a real boy')
 
 new_album.songs.create(name: 'I want to know your plans')
+
+10.times do
+  new_album = Album.create(artist: Faker::Music::RockBand.unique.name, album_title: Faker::Verb.unique.past_participle)
+
+  num_of_songs = rand(3..12)
+
+  num_of_songs.times do
+    new_album.songs.create(name: Faker::Verb.unique.base)
+  end
+end
+
+puts "num of albums #{Album.count}"
+
+puts "num of songs #{Song.count}"
