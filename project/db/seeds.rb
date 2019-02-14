f886e0b5a2b85b09d074894e9ffdb253d6aa0809
Mackenzie <mchoy30@academic.rrc.ca>
2019-02-14 14:35:22 -0600

    Installed faker and updated bundle

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+new_album = Album.create(artist: 'Say anything', album_title: 'Is a real boy')
+
+new_album.songs.create(name: 'I want to know your plans')
