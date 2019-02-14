8ad1f50a74969e4c7e1e71d062e9bb46547b7f9e
Mackenzie <mchoy30@academic.rrc.ca>
2019-02-14 17:05:33 -0600

    Added Faker data

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -10,19 +10,19 @@ new_album.songs.create(name: 'I want to know your plans')
 
 # new_album.album_nformation.create(play_count: 35, date: 'a')
 
- AlbumInformation.create(play_count: 35, date: 'a', album: new_album)
+# AlbumInformation.create(play_count: 35, date: 'a', album: new_album)
 
-# 10.times do
-#   new_album = Album.create(artist: Faker::Music::RockBand.unique.name, album_title: Faker::Verb.unique.past_participle)
-#   num_of_plays = rand(0..35_000_000).to_i
+10.times do
+  new_album = Album.create(artist: Faker::Music::RockBand.unique.name, album_title: Faker::Verb.unique.past_participle)
+  num_of_plays = rand(0..35_000_000).to_i
+  AlbumInformation.create(play_count: num_of_plays, date: Faker::Date.between(2_000_000.days.ago, Date.today), album: new_album)
 
-#   new_album.album_informations.create(play_count: num_of_plays, date: Faker::Date.between(2_000_000.days.ago, Date.today))
-#   num_of_songs = rand(3..12)
+  num_of_songs = rand(3..12)
 
-#   num_of_songs.times do
-#   new_album.songs.create(name: Faker::Verb.unique.base)
-#   end
-# end
+  num_of_songs.times do
+    new_album.songs.create(name: Faker::Verb.unique.base)
+  end
+end
 
 puts "num of albums #{Album.count}"
 
